35fcbe5a99663b217a07faf61594992135d27e21
Wawan <hermawantan12@gmail.com>
2024-02-11 20:39:48 +0800

    finishing api index places

--- app/Http/Controllers/Api/PlaceController.php
+++ app/Http/Controllers/Api/PlaceController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Place;
 use Illuminate\Http\Request;
 
 class PlaceController extends Controller
 {
-    //
+    public function index()
+    {
+        $places = Place::paginate(10);
+
+        return response()->json([
+            'data' => $places
+        ]);
+    }
 }
